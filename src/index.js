d239e9ea9cf1347ad054839e0a97c74e819992c7
Marcel Bensch <mbensch@neverfail.com>
2017-02-20 16:20:20 -0600

    Turn debug mode on again

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Bot from 'core/bot';
 import Skills from 'skills';
 
-const bot = new Bot();
+const bot = new Bot(true);
 
 bot.addSkills(Skills);
 
--- src/skills/hello_world.js
+++ src/skills/hello_world.js
@@ -1,5 +1,6 @@
 export default (controller) => {
     controller.hears(['hello'], ['direct_message','direct_mention'], (bot, message) => {
+        console.info('Received message', message);
         bot.reply(message, "Hello World!");
     });
 };
