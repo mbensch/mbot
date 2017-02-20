50f6930791d14603db5264677eb87acf2541caa1
Marcel Bensch <mbensch@neverfail.com>
2017-02-20 14:27:22 -0600

    Fix hello_world skill

--- src/skills/hello_world.js
+++ src/skills/hello_world.js
@@ -1,5 +1,5 @@
 export default (controller) => {
     controller.hears(['hello'], ['direct_message','direct_mention'], (bot, message) => {
-        bot.reply("Hello World!");
+        bot.reply(message, "Hello World!");
     });
 };
