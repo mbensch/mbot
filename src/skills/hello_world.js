4583fe0085d645a9c78826618f299c9f1a5f2966
Marcel Bensch <mbensch@neverfail.com>
2017-02-20 16:29:34 -0600

    Change triggers from array to string

--- src/skills/hello_world.js
+++ src/skills/hello_world.js
@@ -1,7 +1,7 @@
 export default (controller) => {
     console.log('Adding skill', controller);
 
-    controller.hears(['hello'], ['direct_message','direct_mention'], (bot, message) => {
+    controller.hears(['hello'], 'direct_message,direct_mention,mention', (bot, message) => {
         console.log('Received message', message);
         bot.reply(message, "Hello World!");
     });
