487e3cc0aac6ca121dce7f20d18eb0ebc0006aa2
Marcel Bensch <marcel@message.io>
2017-02-27 13:19:08 -0600

    Debug stuff

--- src/components/webserver/index.js
+++ src/components/webserver/index.js
@@ -1,10 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import each from 'lodash/each';
-import debug from 'debug';
 import * as routes from './routes/index';
-
-const LOG = debug('Bot Webserver');
 
 export default function(controller) {
     const webserver = express();
@@ -13,10 +10,10 @@ export default function(controller) {
     webserver.use(bodyParser.urlencoded({ extended: true }));
     webserver.use(express.static('dist/public'));
 
-    LOG('__dirname', __dirname);
+    console.log('__dirname', __dirname);
 
     webserver.listen(process.env.PORT || 3000, null, function() {
-        LOG('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
+        console.log('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
     });
 
     each(routes, route => route(webserver, controller));
